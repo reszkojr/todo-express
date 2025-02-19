2e34bafaf335799881759108c126e5b4e794648d
Fabio S. Reszko Jr. <s.reszkojr@gmail.com>
2025-02-19 17:13:31 -0300

    update docker-compose

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y postgresql-client
+
+until pg_isready -h postgres -p 5432 -U postgres; do
+    echo "Waiting for PostgreSQL to start..."
+    sleep 2
+done
+
+pnpm run db:generate
+pnpm run db:migrate
+
+pnpm run dev
