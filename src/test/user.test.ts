506f96f9257bb05f6ec659ad710c312c67e05728
Fabio S. Reszko Jr. <s.reszkojr@gmail.com>
2025-02-20 11:28:52 -0300

    user routes testing

---
+++ src/test/user.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('User Routes', () => {
+	let accessToken: string;
+	let refreshToken: string;
+
+	it('should register a new user', async () => {
+		const res = await request(app).post('/api/register').send({
+			username: 'testuser',
+			email: 'testuser@example.com',
+			password: 'password123',
+		});
+		expect(res.statusCode).toEqual(201);
+		expect(res.body).toEqual({
+			id: 1,
+			username: 'testuser',
+			email: 'testuser@example.com',
+		});
+	});
+
+	it('should login an existing user', async () => {
+		const res = await request(app).post('/api/login').send({
+			email: 'testuser@example.com',
+			password: 'password123',
+		});
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toHaveProperty('accessToken');
+		expect(res.body).toHaveProperty('refreshToken');
+
+		refreshToken = res.body.refreshToken;
+		accessToken = res.body.accessToken;
+	});
+
+	it('should reload the access token', async () => {
+		const res = await request(app).post('/api/refresh-token').send({ refreshToken });
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toHaveProperty('accessToken');
+	});
+});
