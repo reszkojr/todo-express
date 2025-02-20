b90ebc8993acd49ead5b366a0f18a7578a10dc73
Fabio S. Reszko Jr. <s.reszkojr@gmail.com>
2025-02-20 12:50:07 -0300

    simple entrypoint feedback

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ until pg_isready -h postgres -p 5432 -U postgres; do
     echo "Waiting for PostgreSQL to start..."
     sleep 2
 done
+echo "PostgreSQL started"
 
 pnpm run db:generate
 pnpm run db:migrate
