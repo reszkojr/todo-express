3c8a1c889421482140d3e9e60d9770c07d1c795f
reszkojr <s.reszkojr@gmail.com>
2025-02-22 14:17:04 -0300

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const server = express();
 const port = process.env.NODE_ENV === 'test' ? 0 : (process.env.PORT || 3000);
 
 server.use(express.json());
+server.use(cors());
+server.options('*', cors());
 
 server.listen(port, () => {
 	console.log(`Server running on port ${port}`);
